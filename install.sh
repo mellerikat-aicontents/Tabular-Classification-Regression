c0f9ace546daf34abcc5685c2bf41fed2a92277f
sehyun.song <sehyun.song@lge.com>
2023-10-27 11:22:25 +0900

    update: Change yaml file to move rather than copy inside ALO

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ git clone http://mod.lge.com/hub/dxadvtech/aicontents-framework/alo.git
 
 echo ""
 echo "***** Copy experimental_plan.yaml to ALO !! *****"
-cp ./experimental_plan.yaml ./alo/config -rf 
+echo 
+mv ./experimental_plan.yaml ./alo/config -f 
 
 
