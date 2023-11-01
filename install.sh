c49763511779b7f24a964ebb3ce5d8310117f127
sehyun.song <sehyun.song@lge.com>
2023-11-02 08:13:29 +0900

    ALO v.1.1.0 이 실행되도록 버전을 고정함

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ## download alo 
 echo "***** Download ALO !! *****"
-git clone http://mod.lge.com/hub/dxadvtech/aicontents-framework/alo.git
+git clone http://mod.lge.com/hub/dxadvtech/aicontents-framework/alo.git -b release-1.1
 
 echo ""
 echo "***** Copy experimental_plan.yaml to ALO !! *****"
