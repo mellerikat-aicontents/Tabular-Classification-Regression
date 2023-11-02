5511d3319e0edfeb826ec8d9c94a53f2f7adf444
sehyun.song <sehyun.song@lge.com>
2023-11-02 11:23:28 +0900

    최신 ALO 가 업데이트 되도록 함 (old contents 는 branch 새로 만들어서 지원 할 예정. 이때 ALO v.1.1.0 이 설치 되도록 함)

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ## download alo 
 echo "***** Download ALO !! *****"
-git clone http://mod.lge.com/hub/dxadvtech/aicontents-framework/alo.git -b release-1.1
+git clone http://mod.lge.com/hub/dxadvtech/aicontents-framework/alo.git
 
 echo ""
 echo "***** Copy experimental_plan.yaml to ALO !! *****"
