fcd518a86d47c8cb2bf46dd4955ce4e942c23951
gy90.moon <gy90.moon@lge.com>
2025-04-22 08:21:30 +0000

    Revert "alo-v3 04/22"
    This reverts commit 9f510bec9f1aee358a1c7452ebd59114d9bc392c.

--- sampling/sampler/nearmiss.py
+++
@@ -1,8 +0,0 @@
-from imblearn.under_sampling import NearMiss
-from .wrapper import Wrapper
-
-
-class TCR_Sampler(Wrapper):
-    def __init__(self, sampler_name, param_dict = {}, config=None):
-        sampler_model = NearMiss(**param_dict)
-        super().__init__(sampler_name, sampler_model, config)
